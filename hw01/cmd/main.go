92c8755bbdf17e93b1c0542c5cbb67dc6daeca11
evgeny <evgen8117@gmail.com>
2020-10-20 20:21:57 +0300

    style: done

--- hw01/cmd/main.go
+++ hw01/cmd/main.go
@@ -10,10 +10,10 @@ func main() {
 	buf := []int{1, 2, 3, 4, 5, 6, 7, 8, 13, 18, 21}
 
 	for _, v := range buf {
-		result, err := fibonacci.Calc(v)
+		result := fibonacci.Calc(v)
 
-		if err != nil {
-			fmt.Println(result, err)
+		if v > 20 {
+			fmt.Println(v, "- аргумент не может быть больше 20")
 			return
 		}
 
--- hw01/pkg/fibonacci/fibonacci.go
+++ hw01/pkg/fibonacci/fibonacci.go
@@ -1,21 +1,16 @@
 package fibonacci
 
-import "fmt"
-
 // Calc вычисление числа Фибоначчи по номеру
-func Calc(number int) (result int, err error) {
-	if number > 20 {
-		return number, fmt.Errorf("- аргумент не может быть больше 20")
-	} else if number == 1 {
-		return 0, nil
+func Calc(number int) (result int) {
+	if number == 1 {
+		return 0
 	}
 
-	firstNumber, result, tempNumber := 0, 1, 0
+	firstNumber, result := 0, 1
 
 	for i := 2; i < number; i++ {
-		tempNumber = result
+		result, firstNumber = firstNumber, result
 		result += firstNumber
-		firstNumber = tempNumber
 	}
-	return result, nil
+	return result
 }
